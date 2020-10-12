72efed11db71d351b5342bd11ea111b5618f2ef8
Taian Fonseca Feitosa <taianmeca@gmail.com>
2020-10-11 21:27:52 -0300

    Refatorar controller

--- src/main/kotlin/br/com/taian/bank/api/controller/APIController.kt
+++ src/main/kotlin/br/com/taian/bank/api/controller/APIController.kt
@@ -61,27 +61,25 @@ class APIController {
         @RequestBody address: Address
     ): ResponseEntity<Response> {
         val validatedAddress = validateAddress(address)
-        val response: ResponseEntity<Response>
-        if (validatedAddress.success) {
+        return if (validatedAddress.success) {
             val client = clientRepository.findByIdOrNull(id)
             if (client != null) {
                 client.address = address
                 clientRepository.save(client)
-                response = ResponseEntity.status(HttpStatus.CREATED)
+                ResponseEntity.status(HttpStatus.CREATED)
                     .header(HttpHeaders.LOCATION, "/client/$id/addDocs")
                     .contentType(MediaType.APPLICATION_JSON)
                     .body(Response(id = client.id))
             } else {
-                response = ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                ResponseEntity.status(HttpStatus.BAD_REQUEST)
                     .contentType(MediaType.APPLICATION_JSON)
                     .body(Response(invalidFields = mapOf("id" to "id not valid")))
             }
         } else {
-            response = ResponseEntity.status(HttpStatus.BAD_REQUEST)
+            ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(Response(invalidFields = validatedAddress.invalidFields))
         }
-        return response
     }
 
     @ApiOperation(value = "Add document to a client")
@@ -91,33 +89,33 @@ class APIController {
         @RequestBody document: Document
     ): ResponseEntity<Response> {
         val validatedDocument = validateDocument(document)
-        val response: ResponseEntity<Response>
-        if (validatedDocument.success) {
-            val client = clientRepository.findByIdOrNull(id)
-            if (client != null) {
-                response = if (client.address == null) {
-                    ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .body(Response(invalidFields = mapOf("address" to "address not set")))
-                } else {
-                    client.document = document
-                    clientRepository.save(client)
-                    ResponseEntity.status(HttpStatus.CREATED)
-                        .header(HttpHeaders.LOCATION, "/client/$id/acceptAccount")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .body(Response(id = client.id))
-                }
-            } else {
-                response = ResponseEntity.status(HttpStatus.NOT_FOUND)
+        val client = clientRepository.findByIdOrNull(id)
+        return when {
+            !validatedDocument.success -> {
+                ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(Response(invalidFields = validatedDocument.invalidFields))
+
+            }
+            client == null -> {
+                ResponseEntity.status(HttpStatus.NOT_FOUND)
                     .contentType(MediaType.APPLICATION_JSON)
                     .body(Response(invalidFields = mapOf("id" to "id not found")))
             }
-        } else {
-            response = ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(Response(invalidFields = validatedDocument.invalidFields))
+            client.address == null -> {
+                ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(Response(invalidFields = mapOf("address" to "address not set")))
+            }
+            else -> {
+                client.document = document
+                clientRepository.save(client)
+                ResponseEntity.status(HttpStatus.CREATED)
+                    .header(HttpHeaders.LOCATION, "/client/$id/acceptAccount")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(Response(id = client.id))
+            }
         }
-        return response
     }
 
     @ApiOperation(value = "View account proposal")
